d0115966fd17efd241da00946df0cd2dc653160f
RaviC Rachamalla <rach0022@algonquinlive.com>
2020-10-24 00:08:52 -0400

    added 10 placeholder sites to GlideImage class

--- app/src/main/java/com/example/randomwebimageapp/GlideImage.kt
+++ app/src/main/java/com/example/randomwebimageapp/GlideImage.kt
@@ -8,9 +8,16 @@ class GlideImage {
 
     //properties of the glideimage class
     private val listOfImageUrls = mutableListOf<String>(
-            "https://placebear.com/800/600",
-            "https://placekitten.com/800/600",
-            "https://placedog.com/800/600"
+        "https://placebear.com/800/600",
+        "https://placekitten.com/800/600",
+        "https://placedog.net/800/600",
+        "https://source.unsplash.com/collection/9435244/800x600",
+        "https://source.unsplash.com/collection/9572295/800x600",
+        "https://source.unsplash.com/collection/181581/800x600",
+        "https://source.unsplash.com/collection/8930154/800x600",
+        "https://picsum.photos/800/600",
+        "https://www.placecage.com/800/600",
+        "http://fillmurray.com/800/600"
     )
 
     private var listCounter = 0
